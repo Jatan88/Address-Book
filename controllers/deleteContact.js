7e50c5150b47fd1571b0ab86be89369db4928242
Jatan Shah <61798906+Jatan88@users.noreply.github.com>
2022-06-02 13:46:34 +0530

    Create deleteContact.js

---
+++ controllers/deleteContact.js
@@ -0,0 +1,19 @@
+const User = require('../models/user')
+const BigPromise = require('../middlewares/bigPromise')
+const CustomError = require('../utils/customError');
+const cookieToken = require('../utils/cookieToken');
+const crypto = require("crypto");
+const user = require('../models/user');
+const contact = require('../models/contact')
+
+exports.deleteContact = BigPromise(async (req, res, next) => {
+    const contact = await Contact.findById(req.params.id);
+
+    if (!contact) {
+      throw new NotFoundError('contact not found');
+    }
+
+    await contact.remove();
+
+    res.send({});
+});
